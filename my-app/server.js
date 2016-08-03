70fa152bdee7ebc0ad9cfed26a9aadabf723d3f1
Aparna Korattyswaroopam <aparna@binarytwine.com>
2016-08-03 17:50:46 +0530

    Basic boiler plate ExpressJS + ReactJS

---
+++ my-app/server.js
@@ -0,0 +1,12 @@
+require('babel-register');
+
+var path = require('path');
+
+var express = require('express');
+var app = express();
+
+app.use('/', express.static('public'));
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
